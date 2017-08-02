a29a129024a156f9ba96be637293929e245e8c11
Akbar Gumbira <akbargumbira@gmail.com>
2017-08-02 17:14:32 +0200

    Move inasafe cli and create setup file.

---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+"""Setup file for distutils / pypi."""
+try:
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+
+setup(
+    name='inasafe_cli',
+    version='4.2.0',
+    py_modules=['inasafe'],
+    license='GPL',
+    author='InaSAFE Team',
+    author_email='info@inasafe.org',
+    url='http://inasafe.org/',
+    description=('Realistic natural hazard impact scenarios for better '
+                 'planning, preparedness and response activities.'),
+    install_requires=[
+        # 'inasafe',
+        'docopt'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GPL V3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+)
